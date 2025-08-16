f082d4d55a8e5d7556ec2ee7f2adf94b7ba7620e
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-16 02:22:12 +0200

    refactor: remove unnecessary comments

--- engine.h
+++ engine.h
@@ -13,33 +13,24 @@ class Enemy;
 
 Position generateNewEnemyPosition(int width, int height);
 
-//#define UNIMPLEMENTED_engineConstructedWithWidthAndHeight
-//#define UNIMPLEMENTED_engineHasGettersToGetInformationAboutPlayer
-//#define UNIMPLEMENTED_engineCanMovePlayer
-//#define UNIMPLEMENTED_engineHandlePlayerShooting
-//#define UNIMPLEMENTED_engineHandlesEnemies
 
 class Engine
 {
 public:
-    /// Konstruktor, który dokona odpowiednich inicjalizacji
     Engine(std::size_t stageWidth, std::size_t stageHeight);
 
-    /// Destruktor (jeśli trzeba)
     ~Engine();
 
     /// metody zwracające informacje o rozmiarze planszy
     std::size_t stageWidthCells() const;
     std::size_t stageHeightCells() const;
 
-    /// zakomentowac makro: UNIMPLEMENTED_engineConstructedWithWidthAndHeight
 
     /// metody zwracające informacje na temat gracza
     Position playerPosition() const; /// domyslnie srodek planszy
     Direction playerDirection() const; /// domyślnie Direction::RIGHT
     bool isPlayerAlive() const;
 
-    /// zakomentowac makro: UNIMPLEMENTED_engineHasGettersToGetInformationAboutPlayer
 
     /// metody obsługiwane z klawiatury
     void playerShoots();
@@ -48,7 +39,6 @@ public:
     void movePlayerLeft();
     void movePlayerRight();
 
-    /// zakomentowac makro: UNIMPLEMENTED_engineCanMovePlayer
 
     /// metody zwracające informacje na temat obiektów w grze
     const std::vector<Bullet>& bullets() const
@@ -56,7 +46,6 @@ public:
         return bullets_;
     }
 
-    /// zakomentowac makro: UNIMPLEMENTED_engineHandlePlayerShooting
 
     /// metoda wywoływania kilka razy na sekundę, która ma za zadanie zaktualizowanie obiektów
     void update();
@@ -72,7 +61,6 @@ protected:
     /// metoda, która wpierw usunie zabitych wrogów, a następnie wykona ruch
     void updateEnemies();
 
-    /// zakomentuj makro: UNIMPLEMENTED_engineHandlesEnemies
 
     /// metoda, która powinna zaktualizować pociski, czyli przesunąć je,
     /// obsłużyć trafienie wroga lub wyjście poza scenę
